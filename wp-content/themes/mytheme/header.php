47e703ce0bc3e7041bd33a9c72d695e709ac3f90
LT User <lianatstaturyan@mail.ru>
2022-03-03 15:48:21 +0400

    Navwalker menu

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -24,25 +24,26 @@
         ?>
     </div>
 
+
+    <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#bs-example-navbar-collapse-1" aria-controls="navbarNav" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+                <?php
+                wp_nav_menu( array(
+                    'theme_location'    => 'top-menu',
+                    'depth'             => 3,
+                    'container'         => 'div',
+                    'container_class'   => 'collapse navbar-collapse',
+                    'container_id'      => 'bs-example-navbar-collapse-1',
+                    'menu_class'        => 'nav navbar-nav mr-auto',
+                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                    'walker'            => new WP_Bootstrap_Navwalker(),
+                ) );
+                ?>
+        </div>
+    </nav>
+
 </header>
 
-<nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
-  <div class="container">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-        <?php
-        wp_nav_menu( array(
-            'theme_location'    => 'top-menu',
-            'depth'             => 3,
-            'container'         => 'div',
-            'container_class'   => 'collapse navbar-collapse',
-            'container_id'      => 'bs-example-navbar-collapse-1',
-            'menu_class'        => 'nav navbar-nav',
-            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-            'walker'            => new WP_Bootstrap_Navwalker(),
-        ) );
-        ?>
-    </div>
-</nav>
